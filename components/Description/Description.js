d24719b01f09cc253adc08ef8b80295f8030d16e
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-06-26 20:18:17 -0300

    logos

--- components/Description/Description.js
+++ components/Description/Description.js
@@ -1,11 +1,24 @@
 import { Title } from "../Common/Title"
 
+import logos from "./LogoArray";
+
+
 export const Description = () => {
     return (
         <>
             <div className='container'>
                 <Title>Sobre Mi</Title>
                 <p align='center'>Hola, mi nombre es Agustín y soy apasionado por la <span>informática</span> y las nuevas tecnologías, enfocandome el apartado WEB. Me encanta crear proyectos personales y compartirlos con mis amigos. A continuación podrá ver alguno de mis trabajos, espero les guste!</p>
+                <div className='logo__container'>
+                    {
+                        logos.map((eachLogo)=>(
+                            <div className='eachIcon' key={eachLogo.title}>
+                                {eachLogo.logo}
+                            </div>
+                        ))
+                    }
+       
+                </div>
                 <div className='line' />
             </div>
             
@@ -14,12 +27,22 @@ export const Description = () => {
                 .container{
                     margin-top: 7rem;
                 }
+                .eachIcon{
+                    margin: 0 0.3rem;
+                }
+                .logo__container{
+                    width:100%;
+                    display:flex;
+                    justify-content:center;
+                    margin-top: 1.5rem;
+                    margin-bottom: 0.3rem;
+                }
 
                 .line{
                     width:60%;
                     height:0.1rem;
                     background: #8170EE;
-                    margin: 1.5rem auto;
+                    margin: 0 auto;
                     border-radius: 8px;
                     margin-bottom: 7rem;
                 }
