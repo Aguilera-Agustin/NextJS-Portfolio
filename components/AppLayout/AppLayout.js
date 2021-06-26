b1f6f008c1a85393bfe131431fe2dc709f6eb1ac
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-06-26 20:41:31 -0300

    seo

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -6,7 +6,7 @@ export const AppLayout = ({children}) => {
         <Head>
             <title>Agustin Aguilera - Portfolio</title>
             <meta charset="UTF-8" key='charset'/>
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" key='equiv'/>
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" key='equiv'/>
             <meta name="description" content="Agustin Aguilera portfolio personal - desarrollador fullstack orientado a WEB." key="description"/>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
 
