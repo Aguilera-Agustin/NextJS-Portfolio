e281972d924b8b0b8bb05858bacf69f8b7173e3b
Agustin Aguilera <98046114+codeagus@users.noreply.github.com>
2022-04-17 21:18:19 -0300

    change job

--- components/Presentation/MainCurrent.js
+++ components/Presentation/MainCurrent.js
@@ -1,35 +1,35 @@
 export const MainCurrent = ({ text }) => {
-    return (
-        <div>
-            <p className='current_text'>
-                📌
-            </p>
-            <p className='current_text'>
-                { text }
-            </p>
-            <style jsx>{`
-                div{
-                    background: #F4F9FB;
-                    width:100%;
-                    height:4em;
-                    display:flex;
-                    align-items: center;
-                    border-radius: 5px;
-                    margin:2rem 0;
-                }
-                p{        
-                    margin: 0.5rem;
-                    font-size: 0.9rem;
-                    font-weight: 500;
-                    color: #37352F;
-                }
-                @media screen and (min-width:768px){
-                    div{
-                        width:40%;
-                    }
-                }
-                `}
-            </style>
-        </div>
-    )
-}
+  return (
+    <div>
+      <p className="current_text">📌</p>
+      <p className="current_text">{text}</p>
+      <style jsx>
+        {`
+          div {
+            background: #f4f9fb;
+            width: 100%;
+            height: 4em;
+            display: flex;
+            align-items: center;
+            border-radius: 5px;
+            margin: 2rem 0;
+          }
+          p {
+            margin: 0.5rem;
+            font-size: 0.9rem;
+            font-weight: 500;
+            color: #37352f;
+          }
+          .current_text {
+            margin-left: 1rem;
+          }
+          @media screen and (min-width: 768px) {
+            div {
+              width: 40%;
+            }
+          }
+        `}
+      </style>
+    </div>
+  );
+};
